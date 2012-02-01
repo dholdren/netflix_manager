a6f96e09ed5b53bfa6912b04bed59872915aa3c8
Dean Holdren <deanholdren@gmail.com>
2012-02-01 12:38:48 -0500

    setting app_name on Netflix client

--- config/initializers/netflix_oauth.rb
+++ config/initializers/netflix_oauth.rb
@@ -3,3 +3,4 @@ require 'netflix'
 
 Netflix::Client.consumer_key = ENV['NETFLIX_OAUTH_DEVELOPER_KEY']
 Netflix::Client.consumer_secret = ENV['NETFLIX_OAUTH_DEVELOPER_SECRET']
+Netflix::Client.app_name = ENV['NETFLIX_OAUTH_APP_NAME'] || 'Profile Manager'
